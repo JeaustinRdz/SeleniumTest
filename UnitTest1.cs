5f7a617d0fae4da2c1fb7e3a6b90b9887299d3c7
Jeaustin Rodriguez Rodriguez (Data Glove IT Solutions Limita) <v-jeaustinro@microsoft.com>
2024-06-17 12:24:06 -0600

    first commit

---
+++ UnitTest1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Edge;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private TestContext testContextInstance;
+        private IWebDriver driver;
+        private string appURL;
+
+
+        [TestMethod]
+        [TestCategory("Firefox")]
+        public void TheBingSearchTest()
+        {
+            driver.Navigate().GoToUrl(appURL);
+            driver.FindElement(By.Id("action-oc18e3")).Click();
+            Assert.IsFalse(driver.Title.Contains("Azure Pipelines"), "Verified title of the page");
+        }
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        [TestInitialize()]
+        public void SetupTest()
+        {
+            appURL = "https://azure.microsoft.com/en-us/";
+
+            string browser = "";
+            switch (browser)
+            {
+                case "Chrome":
+                    driver = new ChromeDriver();
+                    break;
+                case "Firefox":
+                    driver = new FirefoxDriver();
+                    break;
+                case "IE":
+                    driver = new InternetExplorerDriver();
+                    break;
+                default:
+                    driver = new EdgeDriver();
+                    break;
+            }
+
+        }
+
+        [TestCleanup()]
+        public void MyTestCleanup()
+        {
+            driver.Quit();
+        }
+    }
+}
